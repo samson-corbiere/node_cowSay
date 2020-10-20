f478c1db97b023a5e2c9a3e8f522531f0eae9ed5
samson-corbiere <samson.corbiere@gmail.com>
2020-10-20 10:12:23 +0200

    change cowsay index to information

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 const myInformation = require('./information.js');
-myInformation();
+myInformation(); 
+
+
--- information.js
+++ information.js
@@ -1,16 +1,13 @@
 let name = 'Samson';
 let campus = 'Toulouse';
-
-const myInfo = () => {
-    console.log(`Hello I'm ${name} from ${campus} campus!`);
-}
-
-module.exports = myInfo;
-
 var cowsay = require("cowsay");
 
-console.log(cowsay.say({
-	text : `Hello I'm ${name} from ${campus} campus!`,
-	e : "oO",
-	T : "U "
-}));
+const myInfo = () => {
+    console.log(cowsay.say({
+		text : `Hello I'm ${name} from ${campus} campus!`,
+		e : "oO",
+		T : "U "
+	}));
+}
+
+module.exports = myInfo; 
